327f0f71e20372117b1ac0e778a2d934e8dda892
Thomas Schwanberger <schwanberger@gmx.com>
2021-07-12 19:26:06 +0000

    Merging emacs-setup from Windows machine

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Thomas Schwanberger"
+      user-mail-address "thomas@schwanberger.dk")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -21,7 +21,6 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
 (setq doom-font (font-spec :family "Iosevka SS14 Extended" :size 16)
 ;;(setq doom-font (font-spec :family "Iosevka SS14" :size 16)
 ;;(setq doom-font (font-spec :family "Iosevka SS14" :size 16 :weight 'light)
@@ -31,15 +30,25 @@
       ;;doom-variable-pitch-font (font-spec :family "Cantarell" :size 15 :weight 'regular)
       ;;doom-variable-pitch-font (font-spec :family "Source Sans Pro" :size 15 :weight 'semi-bold)
       doom-variable-pitch-font (font-spec :family "Iosevka Etoile" :size 16 :weight 'regular)
-      ;;ivy-posframe-font (font-spec :family "JetBrains Mono" :size 16 :weight 'light)
+      ivy-posframe-font (font-spec :family "JetBrains Mono" :size 16 :weight 'light)
       doom-big-font (font-spec :family "Iosevka Etoile" :size 30)
       )
-
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
+;;(setq doom-theme 'doom-vibrant)
+;;
+;; (setq doom-theme 'doom-gruvbox
+;;       doom-gruvbox-dark-variant 'hard
+;;       )
+
 (setq doom-theme 'doom-one)
+
+;;(setq doom-theme 'doom-solarized-light)
+;;(setq doom-theme 'spacemacs-light)
+;;(setq doom-theme 'doom-dracula)
+;;(setq doom-theme 'doom-nord-light)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -47,7 +56,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 't)
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
@@ -66,3 +75,120 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;; set-locale to DA_DK
+;;(set-locale-environment "DA_DK")
+;; Use UTF-8 for all character encoding.
+;;(set-language-environment 'utf-8)
+;;(set-default-coding-systems 'utf-8)
+;;(set-selection-coding-system 'utf-8)
+;;(prefer-coding-system 'utf-8)
+
+(setq confirm-kill-emacs nil)
+;; WINDOWS SPECIFIC
+;;(setq ispell-program-name "C:/tools/msys64/mingw64/bin/aspell.exe")
+;; (setq plantuml-jar-path "C:/ProgramData/chocolatey/lib/plantuml/tools/plantuml.jar")
+;; (setq org-plantuml-jar-path "C:/ProgramData/chocolatey/lib/plantuml/tools/plantuml.jar")
+;; (setq plantuml-default-exec-mode 'jar)
+;; (setq grip-binary-path "C:/Python39/Scripts/grip.exe")
+
+;; (setq explicit-shell-file-name "C:/WINDOWS/system32/bash.exe")
+;; (setq shell-file-name explicit-shell-file-name)
+;; (add-to-list 'exec-path "C:/WINDOWS/system32")
+;;(add-to-list 'exec-path "C:/tools/msys64/mingw64/bin")
+
+(setq select-enable-clipboard nil)
+
+
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
+
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+(setq org-pandoc-options-for-docx '(
+                                    (reference-doc . "C:/Projects/todo/pandoc/nc_ref.docx")
+                                    ))
+
+(setq org-pandoc-options-for-html5 '(
+                                     (number-sections . t)
+                                      (toc . t)
+                                      (self-contained . t)
+                                      ;(template . "C:/Projects/todo/easy_template.html")
+                                      (template . "C:/Projects/todo/pandoc/html5/github/GitHub.html5")
+                                      ;;(template . "C:/Projects/todo/pandoc/html5/kjhealy/html.template")
+                                     ))
+
+(setq org-pandoc-options-for-latex-pdf '(
+                                         (number-sections . t)
+                                         (toc . t)
+                                         (template . "C:/Projects/todo/eisvogel.tex")
+                                         (pdf-engine . "lualatex")
+                                         ))
+
+
+
+
+(after! org
+  ;;(setq org-superstar-headline-bullets-list '("■" "◆" "▲" "▶")
+
+  ;; A fix to have #begin_src blocks in another color, it is only a problem in doom-vibrant, and should be fixed in the theme instead
+  ;; (require 'color)
+  ;; (set-face-attribute 'org-block nil :background
+  ;;                     (color-darken-name
+  ;;                      (face-attribute 'default :background) 3))
+
+
+  ;;(setq org-superstar-headline-bullets-list '("■" "◆" "▲")
+  ;;(setq org-superstar-headline-bullets-list '("■" "□" "▣" "◆" "◇" "◈")
+  ;;(setq org-superstar-headline-bullets-list '("◳" "◰" "◱" "◲" "◈" "◇" "◆" )
+  ;;(setq org-superstar-headline-bullets-list '("□" "▣" "■" "◇" "◈" "◆")
+  ;;(setq org-superstar-headline-bullets-list '("■" "◆" "▲" "□" "◇" "△")
+  ;;(setq org-superstar-headline-bullets-list '("∷" "∴" "∵")
+  ;;(setq org-superstar-headline-bullets-list '("∮" "∯" "∰" "∎")
+  (setq org-superstar-headline-bullets-list '("☰" "☱" "☲" "☴" "☳" "☶" "☷")
+
+        org-pretty-entities t
+        org-hide-emphasis-markers t ;; show actually italicized text instead of /italicized text/
+        org-agenda-block-separator ""
+        org-fontify-whole-heading-line t
+        org-fontify-done-headline t
+        org-superstar-remove-leading-stars t
+        ;; For some odd reason, this works on windows but not on linux
+        ;; org-superstar-item-bullet-alist '((?* . ?➤)
+        ;;                                   (?+ . ?•)
+        ;;                                   (?- . ?–))
+        org-fontify-quote-and-verse-blocks t)
+
+  (set-face-attribute 'org-document-title nil :font "Iosevka Etoile" :weight 'bold :height 2.0)
+  (dolist (face '((org-level-1 . 1.75)
+                  (org-level-2 . 1.50)
+                  (org-level-3 . 1.25)
+                  (org-level-4 . 1.10)
+                  (org-level-5 . 1.00)
+                  (org-level-6 . 0.90)
+                  (org-level-7 . 0.80)
+                  (org-level-8 . 0.70)))
+    (set-face-attribute (car face) nil :font "Iosevka Etoile" :weight 'bold :height (cdr face)))
+  ;; Make sure org-indent face is available
+  (require 'org-indent)
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  ;; (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  ;; (set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
+  ;; (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  ;; (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  ;; (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+  ;; (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  ;; (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  ;; (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  ;; (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+
+  ;; Get rid of the background on column views
+  (set-face-attribute 'org-column nil :background nil)
+  (set-face-attribute 'org-column-title nil :background nil)
+  )
+
+(load! "+org")
+
+(after! org-tree-slide
+  (advice-remove 'org-tree-slide--display-tree-with-narrow
+                 #'+org-present--hide-first-heading-maybe-a)
+  )
--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -14,27 +14,36 @@
 ;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
 ;;      directory (for easy access to its source code).
 
+;; IGNORE FOR NOW, native comp specific
+;; (defconst dd/using-native-comp (and (fboundp 'native-comp-available-p)
+;;                                     (native-comp-available-p)))
+;; (setq native-comp-deferred-compilation t)
+;; (setq native-comp-async-query-on-exit t)
+;; (setq native-comp-async-jobs-number 8)
+;; (setq native-comp-async-report-warnings-errors nil)
+
 (doom! :input
        ;;chinese
        ;;japanese
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       (company +childframe)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +icons +prescient) ;; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        (emoji +unicode)  ; 🙂
+       ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       (ligatures +extras +iosevka)         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
@@ -42,7 +51,7 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       (treemacs +lsp)          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -65,34 +74,36 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +ranger +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
-       undo              ; persistent, smarter undo for your inevitable mistakes
+       (ibuffer +icons)         ; interactive buffer management
+       (undo +tree)              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       ;;eshell            ; the elisp shell that works everywhere
+       eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
-       syntax              ; tasing you for every semicolon you forget
-       (spell +aspell +everywhere) ; tasing you for misspelling mispelling
-       grammar           ; tasing grammar mistake every you make
+       (syntax +childframe)              ; tasing you for every semicolon you forget
+       ;;(spell +flyspell +hunspell +everywhere) ; tasing you for misspelling mispelling
+       ;;(spell +flyspell +aspell +everywhere) ; tasing you for misspelling mispelling
+       ;;(spell +aspell +everywhere) ; tasing you for misspelling mispelling
+       ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
-       ;;direnv
+       ;;(debugger +lsp)          ; FIXME stepping through code, to help you add bugs
+       direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
-       lookup              ; navigate your code and its documentation
-       ;;lsp               ; M-x vscode
+       (lookup +dictionary)              ; navigate your code and its documentation
+       (lsp +peek)
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -106,18 +117,18 @@
 
        :os
        (:if IS-MAC macos)  ; improve compatibility with macOS
-       tty               ; improve the terminal Emacs experience
+       ;;tty               ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        ;;cc                ; C > C++ == 1
        ;;clojure           ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       ;;data              ; config/data formats
+       data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
@@ -133,7 +144,7 @@
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       ;;json              ; At least it ain't XML
+       (json +lsp)              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
@@ -142,31 +153,31 @@
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+       (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +pretty +present +pandoc)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
-       ;;plantuml          ; diagrams for confusing people more
+       plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
+       (python +lsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (ruby +lsp +rbenv)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
-       sh                ; she sells {ba,z,fi}sh shells on the C xor
+       (sh +lsp +powershell +fish)                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       (yaml +lsp)              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
