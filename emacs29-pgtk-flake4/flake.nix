eb81ac88143f968e3be23853942014ef3a458a28
Thomas Schwanberger <thsc@netcompany.com>
2023-09-28 23:59:09 +0200

    Gotta get dat flake

---
+++ emacs29-pgtk-flake4/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "emacs29-pgtk intended for DOOM Emacs";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";             # primary nixpkgs
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; rec {
+        # Development environment
+        devShell = mkShell {
+          name = "DOOM emacs29-pgtk";
+          nativeBuildInputs = [
+	          ((emacsPackagesFor emacs29-pgtk).emacsWithPackages (epkgs: [epkgs.vterm]))
+            zsh
+	          git
+	          ripgrep
+            bash
+            coreutils
+            openssh
+          ];
+           shellHook = ''
+             exec zsh
+       '';
+        };
+      }
+    );
+}
