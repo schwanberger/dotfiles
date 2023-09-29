e6e5f85c0482a6863c501c00ccf632bb921a383a
Thomas Schwanberger <thsc@netcompany.com>
2023-09-29 20:00:53 +0000

    Update flake

--- emacs29-pgtk-flake4/flake.nix
+++ emacs29-pgtk-flake4/flake.nix
@@ -15,15 +15,23 @@
           name = "DOOM emacs29-pgtk";
           nativeBuildInputs = [
 	          ((emacsPackagesFor emacs29-pgtk).emacsWithPackages (epkgs: [epkgs.vterm]))
-            zsh
 	          git
 	          ripgrep
             bash
             coreutils
             openssh
+            pinentry
+            pinentry-curses
+	    gnupg
+	    nerdfonts
+	    fzf
           ];
            shellHook = ''
-             exec zsh
+             #exec zsh
+      export GPG_TTY=$(tty)
+            echo "DOOOOM"
+            #echo pinentry-program $(which pinentry-curses) >> ~/.gnupg/gpg-agent.conf
+            echo pinentry-program $(which pinentry) >> ~/.gnupg/gpg-agent.conf
        '';
         };
       }
