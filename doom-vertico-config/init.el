8e9443d9e8b4538b8255da89f3ddcfd12c1f8d43
Thomas Schwanberger <thsc@netcompany.com>
2024-02-08 08:27:06 +0100

    module gist no longer exists

--- doom-vertico-config/init.el
+++ doom-vertico-config/init.el
@@ -93,7 +93,7 @@
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       gist              ; interacting with github gists
+       ;;gist              ; interacting with github gists
        ;(lookup +dictionary +offline)              ; navigate your code and its documentation
        (lsp +lsp-ui-peek)               ; M-x vscode
        ;;(lsp +eglot)               ; M-x vscode
